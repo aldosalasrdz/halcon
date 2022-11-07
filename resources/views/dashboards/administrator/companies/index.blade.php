4d3e88a9d3fdf9ab62baa47ecc3034e8f0687cbf
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2022-11-07 01:26:34 -0600

    refactor: companies view

--- resources/views/dashboards/administrator/companies/index.blade.php
+++ resources/views/dashboards/administrator/companies/index.blade.php
@@ -12,23 +12,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="overflow-auto">
-                        <table class="mb-4 text-left w-full">
-                            <thead class="text-sm">
-                                <tr class="border-b">
-                                    <th class="px-4 py-4">Name</th>
-                                    <th class="px-4 py-4">RFC</th>
-                                    <th colspan="2" class="px-4 py-4">Actions</th>
+                    <div class="overflow-auto rounded shadow">
+                        <table class="w-full">
+                            <thead class="bg-green-500 border-b-2 border-green-600 text-white">
+                                <tr>
+                                    <th class="p-3 text-sm font-semibold tracking-wide text-left">#</th>
+                                    <th class="p-3 text-sm font-semibold tracking-wide text-left">Name</th>
+                                    <th class="p-3 text-sm font-semibold tracking-wide text-left">RFC</th>
+                                    <th colspan="2" class="p-3 text-sm font-semibold tracking-wide text-left">Actions</th>
                                 </tr>
                             </thead>
+                            <tbody>
                             @forelse ($companies as $company)
-                            <tr class="border-b border-gray-200 text-sm">
-                                <td class="px-4 py-4 whitespace-nowrap">{{ $company->name }}</td>
-                                <td class="px-4 py-4 whitespace-nowrap">{{ $company->rfc }}</td>
-                                <td class="px-4 py-4 w-5 whitespace-nowrap">
-                                    <a href="{{ route('companies.edit', $company) }}" class="text-indigo-600">Edit</a>
+                            <tr class="border-b">
+                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ $company->id }}</td>
+                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ $company->name }}</td>
+                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ $company->rfc }}</td>
+                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap w-10">
+                                    <a href="{{ route('companies.edit', $company) }}" class="text-indigo-600 hover:underline">Edit</a>
                                 </td>
-                                <td class="px-4 py-4 whitespace-nowrap">
+                                <td class="p-3 text-sm whitespace-nowrap">
                                     <form action="{{ route('companies.destroy', $company) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
@@ -39,13 +42,14 @@
                             </tr>
                             @empty
                             <tr>
-                                <td colspan="3" class="px-4 py-4">
+                                <td colspan="3" class="p-3 text-sm text-gray-700">
                                     No companies found.
                                 </td>
                             </tr>
                             @endforelse
+                            </tbody>
                         </table>
-                    {{ $companies->links() }}
+                        {{ $companies->links() }}
                     </div>
                 </div>
             </div>
