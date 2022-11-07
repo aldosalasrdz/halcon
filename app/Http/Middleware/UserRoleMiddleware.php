547bdf79f0950d081cd14a3818b360ccf0bd5679
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2022-11-07 00:45:36 -0600

    add: user role middleware

--- app/Http/Middleware/UserRoleMiddleware.php
+++ app/Http/Middleware/UserRoleMiddleware.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,9 +15,9 @@ class UserRoleMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, Role $role)
+    public function handle(Request $request, Closure $next, $role)
     {
-        if (Auth::check() && Auth::user()->role == $role)
+        if ($request->user()->hasRole($role))
         {
             return $next($request);
         }
