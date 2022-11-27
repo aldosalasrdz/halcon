2fdfa3ac3c853740ec2d494b0374f3f442bc136f
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2022-11-27 00:50:51 -0600

    add: dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="grid grid-cols-5 gap-4 p-5">
+                        <a class="bg-gray-200 rounded p-4" href="">Administrator &#8599;</a>
+                        <a class="bg-gray-200 rounded p-4" href="">Sales &#8599;</a>
+                        <a class="bg-gray-200 rounded p-4" href="">Purchasing &#8599;</a>
+                        <a class="bg-gray-200 rounded p-4" href="">Warehouse &#8599;</a>
+                        <a class="bg-gray-200 rounded p-4" href="">Route &#8599;</a>
+                    </div>
                 </div>
             </div>
         </div>
